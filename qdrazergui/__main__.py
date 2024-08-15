1618db1bbda0a432a838b3c5ac45d30125076e9e
geezmolycos <djy0212@foxmail.com>
2024-08-15 19:22:27 +0800

    remove textual gui

--- qdrazergui/__main__.py
+++
@@ -1,5 +0,0 @@
-from . import MyApp
-
-if __name__ == '__main__':
-    app = MyApp()
-    app.run()
--- qdrazergui/async_wrapper.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-
-executor = ThreadPoolExecutor(max_workers=10)
-
-# Wrapper to run the blocking function in a thread
-async def async_wrapper(func, *args):
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(executor, func, *args)
